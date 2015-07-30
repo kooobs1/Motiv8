3fa7bb9e0a27d65c4dd2e8abd85aa65cc7506a19
Kaveh Karimiyanha <kavehk3@yahoo.com>
2015-07-30 15:17:42 -0700

    first commit

---
+++ NavigationCell.swift
@@ -0,0 +1,39 @@
+//
+//  NavigationCell.swift
+//  Motiv8
+//
+//  Created by Kaveh Karimiyanha on 7/29/15.
+//  Copyright (c) 2015 Motiv8. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavigationCell: UITableViewCell {
+	
+	@IBOutlet var iconImageView: UIImageView!
+	@IBOutlet var titleLabel: UILabel!
+	@IBOutlet var countLabel: UILabel!
+	@IBOutlet var countContainer: UIView!
+	
+	override func awakeFromNib() {
+		
+		titleLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 20)
+		titleLabel.textColor = UIColor.whiteColor()
+		
+		countLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 16)
+		countLabel.textColor = UIColor.whiteColor()
+		
+		countContainer.backgroundColor = UIColor(red: 0.33, green: 0.62, blue: 0.94, alpha: 1.0)
+		countContainer.layer.cornerRadius = 15
+	}
+	
+	override func setSelected(selected: Bool, animated: Bool) {
+		
+		let countNotAvailable = countLabel.text == nil
+		
+		countContainer.hidden = countNotAvailable
+		countLabel.hidden = countNotAvailable
+		
+	}
+}
