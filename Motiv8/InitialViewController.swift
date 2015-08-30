8a40f716a92119218356c53ebda3aea8255da89a
Kaveh Karimiyanha <kavehk3@yahoo.com>
2015-08-30 14:46:36 -0700

    login is basically working

--- Motiv8/InitialViewController.swift
+++ Motiv8/InitialViewController.swift
@@ -8,16 +8,52 @@
 
 import Foundation
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class InitialViewController: UIViewController {
+class InitialViewController: UIViewController, FBSDKLoginButtonDelegate {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		if (FBSDKAccessToken.currentAccessToken() == nil)
+		{
+			println("Not logged in...")
+		}
+		else
+		{
+			println("Logged in...")
+		}
+		
+		var loginButton = FBSDKLoginButton()
+		loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+		loginButton.center = self.view.center
+		loginButton.delegate = self
+		self.view.addSubview(loginButton)
 	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+	{
+		if error == nil
+		{
+			println("Login complete.")
+			self.performSegueWithIdentifier("presentHome", sender: self)
+		}
+		else
+		{
+			println(error.localizedDescription)
+		}
+		
+	}
+	
+	func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+	{
+		println("User logged out...")
 	}
 	
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
